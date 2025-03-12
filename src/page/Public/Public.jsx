ec422db44c0914a8d414b4b06b3167ef22f5dd09
Shunadesu <namp280918@gmail.com>
2025-03-12 13:56:39 +0700

    check: deloy

--- src/page/Public/Public.jsx
+++ src/page/Public/Public.jsx
@@ -1,12 +1,9 @@
 import { Outlet } from "react-router-dom"
 import { Header, Navbar, Footer } from "../../components"
 import Sidebar from "../../components/Sidebar/Sidebar"
-import BlurText from "../../components/reactbits/BlurText";
 import { ProductIPhoneList, ProductAndroidList, ProductMay99List } from "../../components/pages";
 const Public = () => {
-  const handleAnimationComplete = () => {
-    console.log('Animation completed!');
-  };
+
   return (
     <main className="w-full flex flex-col items-center justify-center gap-4">
         <Header />
@@ -22,16 +19,17 @@ const Public = () => {
         </div>
 
         <div className="max-w-[1440px] flex flex-col justify-center items-center px-8 w-full py-2 relative gap-4">
-          
+          <p className="w-full text-start">Sản phẩm Android</p>
           <ProductAndroidList />
         </div>
 
         <div className="max-w-[1440px] flex flex-col justify-center items-center px-8 w-full py-2 relative gap-4">
-          
+          <p className="w-full text-start">Sản phẩm Iphone</p>
           <ProductIPhoneList />
         </div>
 
         <div className="max-w-[1440px] flex flex-col justify-center items-center px-8 w-full py-2 relative gap-4">
+          <p className="w-full text-start">Máy cũ nhưng xịn xịn</p>
           <ProductMay99List />
         </div>
 
